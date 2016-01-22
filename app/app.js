5a36130da4d0f6410a60420a1187e7d17ef8ae87
Artur Nowakowski <artek700@gmail.com>
2016-01-22 20:50:46 +0100

    cleanup

--- app/app.js
+++ app/app.js
@@ -1,45 +1,40 @@
 'use strict';
 
-var app = angular.module('myApp', ['ngRoute', 'ngTouch'])
+var app = angular.module('myApp', ['ngRoute', 'ngTouch']);
 
-app.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/quiz', {
-    templateUrl: 'app/quizView/quiz.html',
-    controller: 'QuizCtrl'
-  }).otherwise({redirectTo: 'quiz'});
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/quiz', {
+        templateUrl: 'app/quizView/quiz.html',
+        controller: 'QuizCtrl'
+    }).otherwise({redirectTo: 'quiz'});
 }]);
 
 
-app.factory('shuffle', function(){
-        function shuffle(o){
-            if(o) {
-                for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
-                return o;
-            }
-            return o;
+app.factory('shuffle', function () {
+    return function shuffle(o) {
+        if (o) {
+            for (var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
         }
+        return o;
+    };
+});
 
-        return function(arr){
-            shuffle(arr);
+app.factory('Questions', function ($http, $q, shuffle) {
+    return function () {
+        this.questions = [];
+
+        this.load = function () {
+            var self = this;
+            var deferred = $q.defer();
+            $http.get('data.json').then(function (response) {
+                self.questions = response.data.data;
+                deferred.resolve(self.questions);
+            });
+            return deferred.promise;
         };
-    });
-	
-app.factory('Questions', function($http, $q, shuffle){
-        return function(){
-            this.questions = [];
 
-            this.load = function(){
-                var self = this;
-                var deferred = $q.defer();
-                $http.get('data.json').then(function(response){
-                    self.questions = response.data.data;
-                    deferred.resolve(self.questions);
-                });
-                return deferred.promise;
-            };
-
-            this.shuffle = function(){
-                shuffle(this.questions);
-            }
-        }
-    });
+        this.shuffle = function () {
+            shuffle(this.questions);
+        };
+    }
+});
--- app/quizView/QuizCtrl.js
+++ app/quizView/QuizCtrl.js
@@ -1,82 +1,78 @@
 'use strict';
 
-app.controller('QuizCtrl', function($scope, Questions, shuffle, $sce) {
-        $scope.questionIndex = 0;
-        $scope.question = {};
-        $scope.score = 0;
-        $scope.maxScore = 0;
+app.controller('QuizCtrl', function ($scope, Questions, shuffle, $sce) {
+    $scope.questionIndex = 0;
+    $scope.question = {};
+    $scope.score = 0;
+    $scope.maxScore = 0;
 
-        $scope.shuffle = shuffle;
+    $scope.shuffle = shuffle;
 
-        function load() {
-            var q = new Questions();
-            q.load().then(function () {
-                q.shuffle();
-                $scope.questions = q.questions;
-                changeQuestion();
-                $scope.maxScore = $scope.questions.length * 4;
-            });
-        }
-        load();
-
-        $scope.prev  = function(){
-            if($scope.questionIndex > 0) {
-				$scope.clear();
-                $scope.questionIndex--;
-                changeQuestion();
-            }
-        };
-
-        $scope.next = function(){
-            if($scope.questionIndex < $scope.questions.length - 1) {
-				$scope.clear();
-                $scope.questionIndex++;
-                changeQuestion();
-            }
-        };
-
-        $scope.clear = function(){
-            $scope.validationOn = false;
-            angular.forEach($scope.question.answers, function(item){
-                item.checked = false;
-            });
-        };
-
-        function changeQuestion(){
-            $scope.question = $scope.questions[$scope.questionIndex];
-            shuffle($scope.question.answers);
-            angular.forEach($scope.question.answers, function(item) {
-                if(!(typeof(item.answer) == 'object')) {
-                    item.answer = $sce.trustAsHtml(item.answer);
-                }
-            });
-
-        }
-
-        $scope.check = function(){
-            angular.forEach($scope.question.answers, function(item){
-                if((item.checked && item.check==='1') || (!item.checked && item.check === '0') ){
-                    item.ok = true;
-                    if(!$scope.question.validated){
-                        $scope.score++;
-                    }
-                }
-                else{
-                    item.ok = false;
-                }
-            });
-            $scope.question.validated = true;
-            $scope.validationOn = true;
-        };
-
-        $scope.restart = function(){
-            $scope.score = 0;
-            $scope.questionIndex = 0;
-            $scope.validationOn = false;
-            load();
-        };
-
-        $scope.shuffle = function(){
+    function load() {
+        var q = new Questions();
+        q.load().then(function () {
             q.shuffle();
-        };
+            $scope.questions = q.questions;
+            changeQuestion();
+            $scope.maxScore = $scope.questions.length * 4;
+        });
+    }
+
+    load();
+
+    $scope.prev = function () {
+        if ($scope.questionIndex > 0) {
+            $scope.clear();
+            $scope.questionIndex--;
+            changeQuestion();
+        }
+    };
+
+    $scope.next = function () {
+        if ($scope.questionIndex < $scope.questions.length - 1) {
+            $scope.clear();
+            $scope.questionIndex++;
+            changeQuestion();
+        }
+    };
+
+    $scope.clear = function () {
+        $scope.validationOn = false;
+        angular.forEach($scope.question.answers, function (item) {
+            item.checked = false;
+        });
+    };
+
+    function changeQuestion() {
+        $scope.question = $scope.questions[$scope.questionIndex];
+        shuffle($scope.question.answers);
+        angular.forEach($scope.question.answers, function (item) {
+            if (!(typeof(item.answer) == 'object')) {
+                item.answer = $sce.trustAsHtml(item.answer);
+            }
+        });
+    }
+
+    $scope.check = function () {
+        angular.forEach($scope.question.answers, function (item) {
+            if ((item.checked && item.check === '1') || (!item.checked && item.check === '0')) {
+                item.ok = true;
+                if (!$scope.question.validated) {
+                    $scope.score++;
+                }
+            }
+            else {
+                item.ok = false;
+            }
+        });
+        $scope.question.validated = true;
+        $scope.validationOn = true;
+    };
+
+    $scope.restart = function () {
+        $scope.score = 0;
+        $scope.questionIndex = 0;
+        $scope.validationOn = false;
+        load();
+    };
 });
